eea89759213e95b2ef49ec7224e6d315b7539636
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2020-11-18 13:37:10 +0000

    Updates

--- _build0.bat
+++
@@ -1,4 +0,0 @@
-cls
-cd "C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\dmfbsh.github.io"
-cmd /K jekyll build --verbose --config _config0.yml
-pause
--- _build1.bat
+++
@@ -1,7 +0,0 @@
-cls
-cd "C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\dmfbsh.github.io\_ruby"
-ruby convert_md_to_yml.rb
-ruby generate_recents.rb
-cd "C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\dmfbsh.github.io"
-cmd /K jekyll build --verbose --config _config1.yml
-pause
