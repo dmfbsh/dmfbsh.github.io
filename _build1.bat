82d442e5b506df7d8e540f56cb9ab033678de006
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2021-05-13 15:55:55 +0100

    Updates

--- _build1.bat
+++ _build1.bat
@@ -1,11 +1,13 @@
-@ECHO OFF
 
 CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\dmfbsh.github.io\_ruby
 ruby generate_recents.rb
 
 ruby convert_md_to_yml.rb
 
-CD ..
-jekyll build --verbose --config _config1.yml
+CD ..\1shropshire\updates
+CALL jekyll build --verbose --config _config.yml
 
-PAUSE
+COPY _site\1-updateslist.html ..\..\_includes
+
+CD ..\..
+jekyll build --verbose --config _config1.yml
